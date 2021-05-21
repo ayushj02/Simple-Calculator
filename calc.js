cbddaec7241f99da3ca9aedc3fdd06c96f074ee3
ayushj02 <ayushjadhav422@gmail.com>
2021-05-21 14:57:27 +0530

    Added Calculator files (first git commit)

---
+++ calc.js
@@ -0,0 +1,32 @@
+// console.log('Connected To JS Server'); (For Testing Purpose!!)
+
+let display = document.getElementById('display');
+
+let buttons = Array.from(document.getElementsByClassName('buttons'));
+
+buttons.map(button => {
+    button.addEventListener('click' , (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                display.innerText = '';
+                break;
+
+            case '←':
+                if(display.innerText){
+                display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch{
+                    display.innerText = 'Enter A Valid Expression!';
+                }
+                break;
+
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
